ed92876236b54d3d13fc1281051347f81a5a19be
DarioLopes <dariolopes2001@gmail.com>
2023-11-20 18:28:18 -0300

    update 20.11 - mudança no select

--- src/app/pages/inscricao/inscricao.component.ts
+++ src/app/pages/inscricao/inscricao.component.ts
@@ -4,6 +4,16 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+interface Car {
+  value: string;
+  viewValue: string;
+}
 
 
 @Component({
@@ -11,18 +21,34 @@ import {MatButtonModule} from '@angular/material/button';
   templateUrl: './inscricao.component.html',
   styleUrls: ['./inscricao.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, MatButtonModule, CommonModule],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class InscricaoComponent {
   inscritoName:string;
+  selectedValue: string;
+  selectedCar!: string;
   
   constructor(){
     this.inscritoName="";
-  }
+    this.selectedValue="";           
+  } 
 
 enviarinscricao(): void{
   console.log(this.inscritoName);
+  console.log(this.selectedValue)
   }
 
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+
+  cars: Car[] = [
+    {value: 'volvo', viewValue: 'Volvo'},
+    {value: 'saab', viewValue: 'Saab'},
+    {value: 'mercedes', viewValue: 'Mercedes'},
+  ];
 }
